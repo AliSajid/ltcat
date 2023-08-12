8bad721167c6afef8a32d9b3f3dc1fc1d89c1839
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-08-12 13:31:26 -0400

    feat: wrap the feeds route in an AuthCheck

--- src/routes/feeds/+page.svelte
+++ src/routes/feeds/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   //   import type { PageData } from './$types';
 
+  import AuthCheck from '$lib/components/AuthCheck.svelte';
+
   //   export let data: PageData;
 </script>
+
+<AuthCheck></AuthCheck>
