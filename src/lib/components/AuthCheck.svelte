cc7c2ed953641286ad84cdcce013aa22ce5fbf05
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-08-11 22:38:48 -0400

    feat(AuthCheck): add an authcheck component track auth status

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { user } from '$lib/users';
+</script>
+
+{#if $user}
+  <slot />
+{:else}
+  <p class="text-error my-10">You are not authorized to view this page. Please sign in.</p>
+  <p><a class="btn btn-primary btn-wide" href="/login">Sign in</a></p>
+{/if}
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+  import AuthCheck from '$lib/components/AuthCheck.svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
 </script>
 
-<div class="card w-96 card-normal text-primary-content">
-  <div class="card-body items-center text-center">
-    <h1 class="card-title">Profile</h1>
-    <p class="card-subtitle">Welcome to your profile page</p>
+<AuthCheck>
+  <div class="card w-96 card-normal text-primary-content">
+    <div class="card-body items-center text-center">
+      <h1 class="card-title">Profile</h1>
+      <p class="card-subtitle">Welcome to your profile page</p>
+    </div>
   </div>
-</div>
+</AuthCheck>
