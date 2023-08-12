fcc5a974a6ecb6ded0c57be2a10fd1295785eb7e
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-08-11 23:26:45 -0400

    feat(NavBar): add more links to the NavBar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,4 +1,14 @@
+<script>
+  import { user } from '$lib/users';
+</script>
+
 <nav class="navbar bg-primary text-secondary-content">
   <a href="/" class="btn btn-ghost normal-case text-xl">Home</a>
+  <a href="/profile" class="btn btn-ghost normal-case text-xl">Profile</a>
   <a href="/feeds" class="btn btn-ghost normal-case text-xl">Feeds</a>
+  {#if user}
+    <a href="/logout" class="btn btn-ghost normal-case text-xl">Logout</a>
+  {:else}
+    <a href="/login" class="btn btn-ghost normal-case text-xl">Login</a>
+  {/if}
 </nav>
