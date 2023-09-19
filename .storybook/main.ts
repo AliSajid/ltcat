711f4ca86596cced90024d34e4cbabbdfa3e82f5
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-09-18 23:45:17 -0400

    build: remove exta config from storybook plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,19 +2,7 @@ import type { StorybookConfig } from '@storybook/sveltekit';
 
 const config: StorybookConfig = {
   'stories': ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  'addons': [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        postCss: {
-          implementation: require.resolve('postcss'),
-        },
-      },
-    },
-  ],
+  'addons': ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   'framework': {
     'name': '@storybook/sveltekit',
     'options': {},
