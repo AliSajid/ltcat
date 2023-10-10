f3a12402a0e03adaec8667b487743a80ae52411f
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-10-10 16:22:36 -0400

    feat: add a feeds route with dummy data

---
+++ src/lib/components/PodcastCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+  export let image: string;
+</script>
+
+<div class="card card-bordered">
+  <div class="card-body">
+    <img src={image} alt="Title of the Podcast" class="image-full" />
+    <div class="p-4">
+      <h2 class="card-title">{title}</h2>
+      <p class="card-body">{description}</p>
+    </div>
+  </div>
+</div>
--- src/routes/feeds/+page.svelte
+++ src/routes/feeds/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
-  //   import type { PageData } from './$types';
+  import type { PageData } from './$types';
+  import PodcastCard from '$lib/components/PodcastCard.svelte';
 
   import AuthCheck from '$lib/components/AuthCheck.svelte';
 
-  //   export let data: PageData;
+  export let data: PageData;
 </script>
 
-<AuthCheck></AuthCheck>
+<AuthCheck>
+  <h1 class="text-3xl">Feeds</h1>
+  <div class="flex flex-wrap -mx-2">
+    {#each data.feeds as feed}
+      <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 py-2">
+        <PodcastCard title={feed.title} description={feed.description} image={feed.image} />
+      </div>
+    {/each}
+  </div></AuthCheck
+>
