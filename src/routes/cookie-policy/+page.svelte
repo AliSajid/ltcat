9b91c96444d153892c6c631a0eec202f4bdc26b2
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-10-18 15:04:55 -0400

    feat: add cookie policy
    closes #6

---
+++ src/routes/cookie-policy/+page.svelte
@@ -0,0 +1,44 @@
+<div class="p-6">
+  <h1 class="text-2xl font-bold mb-4">Cookie Policy</h1>
+
+  <p class="mb-4">Last Updated: <span class="italic text-accent">October 15, 2023</span></p>
+
+  <p class="mb-4">
+    This Cookie Policy explains how we use cookies and similar tracking technologies on our website and application.
+  </p>
+
+  <h2 class="text-xl font-semibold mb-2">1. What Are Cookies?</h2>
+  <p class="mb-4">
+    Cookies are small text files that are stored on your device when you visit a website. They are widely used to make
+    websites work, improve user experience, and provide information to website owners.
+  </p>
+
+  <h2 class="text-xl font-semibold mb-2">2. How We Use Cookies</h2>
+  <p class="mb-4">We use cookies for the following purposes:</p>
+  <ul class="list-disc list-inside mb-4">
+    <li>To authenticate and track user sessions.</li>
+    <li>To remember user preferences and settings.</li>
+    <li>To analyze user behavior and improve our website and application.</li>
+  </ul>
+
+  <h2 class="text-xl font-semibold mb-2">3. Types of Cookies We Use</h2>
+  <p class="mb-4">We use the following types of cookies:</p>
+  <ul class="list-disc list-inside mb-4">
+    <li><strong>Session Cookies:</strong> These are temporary cookies that expire when you close your browser.</li>
+    <li><strong>Persistent Cookies:</strong> These cookies remain on your device for a specified period.</li>
+  </ul>
+
+  <h2 class="text-xl font-semibold mb-2">4. Cookie Management</h2>
+  <p class="mb-4">
+    You can manage cookie preferences in your web browser settings. You may choose to accept or decline cookies.
+    However, disabling cookies may affect your user experience.
+  </p>
+
+  <h2 class="text-xl font-semibold mb-2">5. Contact Us</h2>
+  <p class="mb-4">
+    If you have any questions or concerns regarding our Cookie Policy, please contact us at <a
+      href="mailto:ltcat.cookies@imamiland.com"
+      class="link link-info">ltcat.cookies@imamiland.com</a
+    >.
+  </p>
+</div>
