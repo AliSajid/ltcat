7ca35bd7867676e442a1b249f726c3a61ef87529
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-10-18 17:55:59 -0400

    style: fix style

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -18,7 +18,7 @@
   </p>
 
   <h3>Google Analytics</h3>
-  <p>
+  <p></p>
   <h3 class="text-lg font-semibold mb-2">2.2. Google Analytics</h3>
   <p class="mb-4">
     We use Google Analytics, a web analytics service provided by Google Inc. Google Analytics collects information such
@@ -28,63 +28,61 @@
   <h2>How We Use Your Information</h2>
   <p>We use the collected information for the following purposes:</p>
   <ul>
-  <h2 class="text-xl font-semibold mb-2">3. How We Use Your Information</h2>
-  <p class="mb-4">We use the collected information for the following purposes:</p>
-  <ul class="list-disc list-inside mb-4">
-    <li>To provide and maintain our application.</li>
-    <li>To improve the user experience and functionality.</li>
-    <li>To understand how users interact with our app.</li>
-    <li>To ensure the security of user data and the application.</li>
+    <h2 class="text-xl font-semibold mb-2">3. How We Use Your Information</h2>
+    <p class="mb-4">We use the collected information for the following purposes:</p>
+    <ul class="list-disc list-inside mb-4">
+      <li>To provide and maintain our application.</li>
+      <li>To improve the user experience and functionality.</li>
+      <li>To understand how users interact with our app.</li>
+      <li>To ensure the security of user data and the application.</li>
+    </ul>
+
+    <h2 class="text-xl font-semibold mb-2">4. Cookies</h2>
+    <p class="mb-4">
+      We use cookies to track authentication status and enhance your experience when using our application. You can
+      manage cookie preferences in your web browser settings.
+    </p>
+
+    <h2 class="text-xl font-semibold mb-2">5. Third-Party Services</h2>
+    <p class="mb-4">
+      We use Google Analytics to collect and analyze user data. Google Analytics may collect and process data as
+      described in their Privacy Policy. To understand how Google Analytics uses data, please visit Google's Privacy
+      Policy.
+    </p>
+
+    <h2 class="text-xl font-semibold mb-2">6. Data Retention</h2>
+    <p class="mb-4">
+      We retain your data only for as long as necessary for the purposes outlined in this Privacy Policy.
+    </p>
+
+    <h2 class="text-xl font-semibold mb-2">7. Data Security</h2>
+    <p class="mb-4">
+      We take reasonable measures to protect your data. However, please be aware that no method of data transmission or
+      storage is entirely secure.
+    </p>
+
+    <h2 class="text-xl font-semibold mb-2">8. Your Rights</h2>
+    <p class="mb-4">You have the right to:</p>
+    <ul class="list-disc list-inside mb-4">
+      <li>Access and receive a copy of your data.</li>
+      <li>Request rectification of your data.</li>
+      <li>Request the deletion of your data.</li>
+      <li>Object to data processing.</li>
+    </ul>
+
+    <h2 class="text-xl font-semibold mb-2">9. Changes to This Privacy Policy</h2>
+    <p class="mb-4">
+      We may update this Privacy Policy to reflect changes in our data processing practices or for legal reasons. Users
+      will be notified of any significant changes. Continued use of our application constitutes your acceptance of the
+      updated Privacy Policy.
+    </p>
+
+    <h2 class="text-xl font-semibold mb-2">10. Contact Us</h2>
+    <p class="mb-4">
+      If you have any questions, concerns, or requests regarding this Privacy Policy, please contact us at <a
+        href="mailto:ltcat.privacy@imamiland.com"
+        class="link link-info">ltcat.privacy@imamiland.com</a
+      >.
+    </p>
   </ul>
-
-  <h2 class="text-xl font-semibold mb-2">4. Cookies</h2>
-  <p class="mb-4">
-    We use cookies to track authentication status and enhance your experience when using our application. You can manage
-    cookie preferences in your web browser settings.
-  </p>
-
-  <h2 class="text-xl font-semibold mb-2">5. Third-Party Services</h2>
-  <p class="mb-4">
-
-    We use Google Analytics to collect and analyze user data. Google Analytics may collect and process data as described
-    in their Privacy Policy. To understand how Google Analytics uses data, please visit Google's Privacy Policy.
-  </p>
-
-  <h2 class="text-xl font-semibold mb-2">6. Data Retention</h2>
-  <p class="mb-4">
-    We retain your data only for as long as necessary for the purposes outlined in this Privacy Policy.
-  </p>
-
-  <h2 class="text-xl font-semibold mb-2">7. Data Security</h2>
-  <p class="mb-4">
-    We take reasonable measures to protect your data. However, please be aware that no method of data transmission or
-    storage is entirely secure.
-  </p>
-
-
-  <h2 class="text-xl font-semibold mb-2">8. Your Rights</h2>
-  <p class="mb-4">You have the right to:</p>
-  <ul class="list-disc list-inside mb-4">
-    <li>Access and receive a copy of your data.</li>
-    <li>Request rectification of your data.</li>
-    <li>Request the deletion of your data.</li>
-    <li>Object to data processing.</li>
-  </ul>
-
-
-  <h2 class="text-xl font-semibold mb-2">9. Changes to This Privacy Policy</h2>
-  <p class="mb-4">
-
-    We may update this Privacy Policy to reflect changes in our data processing practices or for legal reasons. Users
-    will be notified of any significant changes. Continued use of our application constitutes your acceptance of the
-    updated Privacy Policy.
-  </p>
-
-  <h2 class="text-xl font-semibold mb-2">10. Contact Us</h2>
-  <p class="mb-4">
-    If you have any questions, concerns, or requests regarding this Privacy Policy, please contact us at <a
-      href="mailto:ltcat.privacy@imamiland.com"
-      class="link link-info">ltcat.privacy@imamiland.com</a
-    >.
-  </p>
 </div>
